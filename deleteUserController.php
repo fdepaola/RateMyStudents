4a6d70f67a4a5b9e4f00f5c0c07c497680c167fb
heWhoCodes <sjordan@mail.umw.edu>
2013-03-18 14:17:46 -0300

    Update deleteUserController.php
    Changed parameters

--- deleteUserController.php
+++ deleteUserController.php
@@ -3,10 +3,10 @@
 	$db = mysqli_connect('localhost', 'root', '', 'ratemystudents')
 		or die(mysqli_error($db));
 
-	$first_name = $_POST['first_name'];
-	$last_name = $_POST['last_name'];
+	$user_email = $_POST['user_email'];
+	$user_password = $_POST['user_password'];
 	
-	$query = "DELETE FROM user WHERE first_name='$first_name' AND last_name='$last_name'";
+	$query = "DELETE FROM user WHERE email='$user_email' AND password='$user_password'";
 	$result = mysqli_query($db, $query) or die(mysqli_error($db));
 
 	header("Location: loginView.php");
