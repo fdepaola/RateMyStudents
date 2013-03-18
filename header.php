8f24520b8202ee5169c59ddc235e8a589f98fe43
Michael Crawford <mscrawfor@gmail.com>
2013-03-18 18:29:10 -0400

    commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<div id="templatemo_header">
+	<h2>rate my students</h2>
+</div>
+	
+<div id="templatemo_menu">
+	<ul>
+        <li><a href="home.php">home</a></li>
+        <li><a href="addStudentView.php">add student</a></li>
+        <li><a href="deleteUserView.php">delete user</a></li>
+        <li><a href="loginView.php">log in</a></li>
+        <li><a href="logoutController.php" class="last">log out</a></li>
+	</ul>       
+</div>
