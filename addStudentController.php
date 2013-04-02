2349ec906611fbe01adcb5f8c1cb88cf758e34d9
samJordan <sjordan@mail.umw.edu>
2013-04-01 21:41:23 -0300

    Implimented prepared statements

--- addStudentController.php
+++ addStudentController.php
@@ -2,13 +2,19 @@
 	session_start();
 	$db = mysqli_connect('localhost', 'root', '', 'ratemystudents')
 		 or die(mysqli_error($db));
-
+	
+	$query = $db->prepare("INSERT INTO student(`last_name`, `first_name`, `major`) 
+		VALUES (?, ?, ?)");
+	
+	$query->bindparam(1,$lastname);
+	$query->bindparam(2,$firstname);
+	$query->bindparam(3,$major);
+	
 	$firstname = $_POST['firstname'];
 	$lastname = $_POST['lastname'];
 	$major = $_POST['major'];
 	
-	$query = "INSERT INTO student(`last_name`, `first_name`, `major`) 
-		VALUES ('$lastname', '$firstname', '$major')";
+	$query->execute();
 
 	$result = mysqli_query($db, $query) or die(mysqli_error($db));
 
